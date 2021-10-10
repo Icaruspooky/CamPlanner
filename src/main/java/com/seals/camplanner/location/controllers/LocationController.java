f274c561e2fb03b933598bfb34ca5e9a5c2dee63
Icaro Cristofolini <icaroaj@hotmail.com>
2021-10-10 18:09:04 +0100

    Created Location endpoints

--- src/main/java/com/seals/camplanner/location/controllers/LocationController.java
+++ src/main/java/com/seals/camplanner/location/controllers/LocationController.java
@@ -3,8 +3,7 @@ package com.seals.camplanner.location.controllers;
 import com.seals.camplanner.location.models.Location;
 import com.seals.camplanner.location.services.LocationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +15,20 @@ public class LocationController {
     @GetMapping("/location")
     public List<Location> getLocations() {
         return locationService.findAll();
+    }
+
+    @PostMapping("/event")
+    public Location saveEvent(@RequestBody Location location) {
+        return locationService.save(location);
+    }
+
+    @GetMapping("/event/{id}")
+    public Location getEvent(@PathVariable("id") Long id) {
+        return locationService.find(id);
+    }
+
+    @DeleteMapping("/event/{id}")
+    public void deleteEvent(@PathVariable("id") Long id) {
+        locationService.delete(id);
     }
 }
