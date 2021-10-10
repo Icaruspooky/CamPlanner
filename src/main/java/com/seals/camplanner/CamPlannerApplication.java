a4e00b0446560d18e5052ba1906a573368a4de30
Icaro Cristofolini <icaroaj@hotmail.com>
2021-10-10 14:37:27 +0100

    Init CamPlanner

---
+++ src/main/java/com/seals/camplanner/CamPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.seals.camplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seals/camplanner/CamPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seals.camplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
