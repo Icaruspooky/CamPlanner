767f5472a485b60aa0b566063162845e8253e15b
Icaro Cristofolini <icaroaj@hotmail.com>
2021-10-16 22:27:10 +0100

    CMPN-4 - Created DTO structure for the "event" endpoint

--- src/main/java/com/seals/camplanner/event/controllers/EventController.java
+++ src/main/java/com/seals/camplanner/event/controllers/EventController.java
@@ -3,34 +3,55 @@ package com.seals.camplanner.event.controllers;
 import com.seals.camplanner.event.dto.EventDto;
 import com.seals.camplanner.event.models.Event;
 import com.seals.camplanner.event.services.EventService;
+import com.seals.camplanner.location.models.Location;
+import com.seals.camplanner.location.services.LocationService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
+    private final LocationService locationService;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/event")
-    public List<Event> getEvents() {
-        return eventService.findAll();
+    public List<EventDto> getEvents() {
+        return toEventDtoList(eventService.findAll());
     }
 
     //TODO add treatment for the "location" parameter
     @PostMapping("/event")
-    public Event saveEvent(@RequestBody EventDto event) {
-        return eventService.save(event);
+    public EventDto saveEvent(@RequestBody EventDto event) {
+        return toDto(eventService.save(fromDto(event)));
     }
 
     @GetMapping("/event/{id}")
-    public Event getEvent(@PathVariable("id") Long id) {
-        return eventService.find(id);
+    public EventDto getEvent(@PathVariable("id") Long id) {
+        return toDto(eventService.find(id));
     }
 
     @DeleteMapping("/event/{id}")
     public void deleteEvent(@PathVariable("id") Long id) {
         eventService.delete(id);
+    }
+
+    private EventDto toDto(Event event) {
+        return modelMapper.map(event, EventDto.class);
+    }
+
+    private Event fromDto(EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
+        Location location = locationService.find(eventDto.getLocation());
+        event.setLocation(location);
+        return event;
+    }
+
+    private List<EventDto> toEventDtoList(List<Event> events) {
+        return events.stream().map(event -> modelMapper.map(event, EventDto.class)).collect(Collectors.toList());
     }
 }
