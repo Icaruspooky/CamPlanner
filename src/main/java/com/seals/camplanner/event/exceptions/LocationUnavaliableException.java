43d001c302227951b32035a51927295eadbf2065
Icaro Cristofolini <icaroaj@hotmail.com>
2021-10-26 19:32:14 +0100

    CMPN-9 - Implementation of Logs, Exceptions and correct responses

---
+++ src/main/java/com/seals/camplanner/event/exceptions/LocationUnavaliableException.java
@@ -0,0 +1,11 @@
+package com.seals.camplanner.event.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class LocationUnavaliableException extends RuntimeException{
+    public LocationUnavaliableException(Long id) {
+        super(String.format("The id (%s) provided for the Location is not available.", id));
+    }
+}
--- src/main/java/com/seals/camplanner/location/controllers/LocationController.java
+++ src/main/java/com/seals/camplanner/location/controllers/LocationController.java
@@ -5,6 +5,7 @@ import com.seals.camplanner.location.models.Location;
 import com.seals.camplanner.location.services.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,8 +32,15 @@ public class LocationController {
     }
 
     @DeleteMapping("/location/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteLocation(@PathVariable("id") Long id) {
         locationService.deleteById(id);
+    }
+
+    @DeleteMapping("/location")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteLocations() {
+        locationService.deleteAll();
     }
 
     private LocationDto toDto(Location location) {
